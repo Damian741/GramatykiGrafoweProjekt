86b78b1054777c193c9094d650a662c61de4cb6a
blagosz <lagosz.bartlomiej@gmail.com>
2021-12-15 02:18:46 +0100

    Bugged implementation of production P1

--- GraphFragment.py
+++ GraphFragment.py
@@ -1,5 +1,7 @@
 class GraphFragment:
-    def __init__(self, squares, verticies, layer_number):
+    def __init__(self, squares, verticies, layer_number, edges, middle_verticies):
         self.squares = squares
         self.verticies = verticies
         self.layer_number = layer_number
+        self.edges = edges
+        self.middle_verticies = middle_verticies
--- Square.py
+++ Square.py
@@ -1,4 +1,5 @@
 class Square:
-    def __init__(self, square_id, layer_number):
-        self.square_id = square_id
+    def __init__(self, field_id, layer_number):
+        self.field_id = field_id
         self.layer_number = layer_number
+        self.verticies = []

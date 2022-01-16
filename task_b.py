edabc2ab57acdd2eb397bdac4beabf53b48b2211
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2022-01-16 14:20:51 +0100

    Add P10

---
+++ task_b.py
@@ -0,0 +1,13 @@
+from util.GraphDrawer import draw_graph
+from productions.P1 import P1
+from productions.P2 import P2
+from productions.P10 import P10
+
+P1(0)
+P2(5)
+P10(10)
+P2(15)
+P2(20)
+P2(25)
+
+draw_graph()
